8b1865aea7aa17824408b2563e48b0a6b368848d
Nicomedinapozzi <nicolas.medina.pozzi@gmail.com>
2022-08-31 09:05:25 -0300

    Desafio1

---
+++ Desafios/Desafio1/index.js
@@ -0,0 +1,10 @@
+class usuario {
+    constructor(nombre, apellido, libros, mascotas){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.libros = libros;
+        this.mascotas = mascotas;
+      }
+
+}
+
