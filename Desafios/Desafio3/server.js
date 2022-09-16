3924fc50db121fa784a812258151cf23d6cd3887
Nicomedinapozzi <nicolas.medina.pozzi@gmail.com>
2022-09-15 22:21:12 -0300

    desafio3final

--- Desafios/Desafio3/server.js
+++ Desafios/Desafio3/server.js
@@ -4,17 +4,17 @@ const app = express()
 const contenedor = require('./contenedor')
 const PORT = 8080
 
-const products = new contenedor('./productos.txt')
+const productos = new contenedor('./productos.txt')
 
 app.listen(PORT, ()=>console.log(`Se escucha puerto: ${PORT}`) )
 
 
-app.get('./products', (req, res) => {
-    res.json(products.getAll())
+app.get('./productos', (req, res) => {
+    res.json(productos.getAll())
   
   })
 
   app.get('/random', (req, res) => {
-    res.json(products.getRandom())
+    res.json(productos.getRandom())
   
   })
